2ecfa18472a4807b387d4cd4f2c419d17d5d32e8
DARSHAN G <42838584+darshangopal0904@users.noreply.github.com>
2020-07-11 11:46:12 +0530

    diff_ques_find using decision tree

---
+++ Diff_ques_finder/diff_ques_finder_decision_tree/model.py
@@ -0,0 +1,44 @@
+# import pandas
+import pandas as pd
+
+# read the dataset
+df = pd.read_csv('C:/Users/DARSHAN.G/Downloads/dataset_examly_2.csv', error_bad_lines=False)
+
+df.head()
+
+#print(df.shape)
+
+num_of_classes = len(df.Difficulty.unique())
+#print(num_of_classes)
+
+df.describe()
+
+# split train input and output data
+X = df.drop(axis=0, columns=['Difficulty'])
+Y = df.Difficulty
+
+#Print the shape of X and Y
+#print(X.shape)
+#print(Y.shape)
+
+from sklearn.model_selection import train_test_split
+
+# Split into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+
+from sklearn.tree import DecisionTreeClassifier 
+dtree_model = DecisionTreeClassifier(max_depth = 2).fit(X_train, y_train) 
+
+
+# Save your model
+from sklearn.externals import joblib
+joblib.dump(dtree_model, 'model.pkl')
+print("Model dumped!")
+
+# Load the model that you just saved
+model = joblib.load('model.pkl')
+
+# Saving the data columns from training
+model_columns = list(X.columns)
+joblib.dump(model_columns, 'model_columns.pkl')
+print("Models columns dumped!")
